3083cdb937706193b1e800f62279f0f8e18d9e58
wangjie <jiebupup@gmail.com>
2022-01-22 13:37:36 +0800

    compeleted

---
+++ src/testCases.java
@@ -0,0 +1,21 @@
+/*
+import com.wind.CEDCS;
+
+public class testCases {
+    // 随机生成车辆位置
+
+    // 根据车辆位置
+    //
+    public static void main(String[] args) {
+        int dim = 10;
+        for (int i = 0; i < 1; i++) {
+//            int[][] matrix = CEDCS.GenRandomSquareMatrix(dim, 1, 4);
+//            System.out.println(CEDCS.Matix2Str(matrix));
+//            double[][] car_time = CEDCS.GenRandomCarInterval(dim, 0, 5, 15, 20);
+//            System.out.println(CEDCS.Matix2Str(car_time));
+            int[][] matrix = GA.GenRandomSquareMatrix();
+            System.out.println(GA.Matix2Str(matrix));
+        }
+    }
+}
+*/
